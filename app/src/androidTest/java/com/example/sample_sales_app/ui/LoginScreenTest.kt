2497d4baef2da83aaef71dcc30fc3eaf59ef25a4
Xoan Gonzalez <xoangon@gmail.com>
2022-04-15 15:42:59 +0200

    UI tests must be in androidTest folder

--- app/src/androidTest/java/com/example/sample_sales_app/ui/LoginScreenTest.kt
+++ app/src/androidTest/java/com/example/sample_sales_app/ui/LoginScreenTest.kt
@@ -1,10 +1,15 @@
-package com.example.sample_sales_app.view_model.ui
+package com.example.sample_sales_app.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 import org.junit.Rule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import com.example.sample_sales_app.screens.LoginScreen
-import com.example.sample_sales_app.screens.modifier
+import com.example.sample_sales_app.ui.theme.SampleSalesAppTheme
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -16,9 +21,11 @@ class LoginScreenTest {
 
    @DisplayName("Should have welcome text")
    @Test
-   fun `login screen has the welcome text`() {
+   fun login_screen_has_the_welcome_text() {
        composeTestRule.setContent {
-          LoginScreen(modifier)
+           SampleSalesAppTheme {
+               LoginScreen(Modifier.fillMaxSize())
+           }
        }
        val text = composeTestRule.onNodeWithTag("Welcome text").fetchSemanticsNode().config
        text shouldBe "Welcome to Sample Sales Application!"
