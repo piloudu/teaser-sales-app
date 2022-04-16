f7c3186679bdaad9877a454f1241f0b50baa1e43
Xoan Gonzalez <xoangon@gmail.com>
2022-04-16 08:17:03 +0200

    Create infix method to change currencies

--- app/src/main/java/com/example/sample_sales_app/utils/AppUtils.kt
+++ app/src/main/java/com/example/sample_sales_app/utils/AppUtils.kt
@@ -2,6 +2,8 @@ package com.example.sample_sales_app.utils
 
 import android.widget.Toast
 import com.example.sample_sales_app.MainActivity
+import com.example.sample_sales_app.data_model.Currency
+import com.example.sample_sales_app.get_data.Cache
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
@@ -10,4 +12,16 @@ internal fun <T> String.deserialize(): List<T> = mapper.readValue(this)
 
 fun toastMessage(message: String) {
     Toast.makeText(MainActivity.getContext(), message, Toast.LENGTH_SHORT).show()
+}
+
+suspend infix fun Currency.to(currency: Currency): String {
+    val currencyChanges = Cache.get().currencyChanges
+    var rate = ""
+    currencyChanges.find { it.from == this.name && it.to == currency.name }?.let {
+        return it.rate
+    }
+    currencyChanges.find { it.from == currency.name && it.to == this.name }?.let {
+        rate = (1/it.rate.toDouble()).toString()
+    }
+    return rate
 }
