e974d61786df300b1cfdac36d027d2e403fd3881
Xoan Gonzalez <xoangon@gmail.com>
2022-04-16 22:30:05 +0200

    Add TopBar to MainScreen

--- app/src/main/java/com/example/sample_sales_app/screens/MainScreen.kt
+++ app/src/main/java/com/example/sample_sales_app/screens/MainScreen.kt
@@ -1,28 +1,33 @@
 package com.example.sample_sales_app.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sample_sales_app.ui.theme.Black
+import com.example.sample_sales_app.ui.theme.Purple700
+import com.example.sample_sales_app.ui.theme.White
 import com.example.sample_sales_app.view_model.MainViewModel
 
 enum class MainScreenTags {
     DROPDOWN_INSTRUCTIONS, DROPDOWN, CURRENCY_INSTRUCTIONS, EUR, AUD, USD, CAD, RESULT
 }
 
-enum class MainScreenMessages {
-    DROP_DOWN_INSTRUCTIONS,
+enum class MainScreenMessages(val message: String) {
+    HEADER("Sample Sales App"),
+    DROPDOWN_INITIAL("Select an order")
 }
 
 @Preview(showBackground = true)
@@ -36,12 +41,31 @@ fun MainScreen(
     modifier: Modifier
 ) {
     Column(
-        modifier.fillMaxSize(),
+        modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
+        TopBar(modifier = Modifier.height(32.dp))
+        Spacer(Modifier.weight(1f))
         DropdownPanel(
-            modifier = Modifier.testTag(MainScreenTags.DROPDOWN.name)
+            modifier = Modifier
+                .testTag(MainScreenTags.DROPDOWN.name)
+                .weight(2f)
+        )
+    }
+}
+
+@Composable
+fun TopBar(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .background(Purple700)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = MainScreenMessages.HEADER.message,
+            fontSize = 20.sp,
+            color = White
         )
     }
 }
@@ -52,24 +76,31 @@ fun DropdownPanel(
 ) {
     val state by MainViewModel.state.collectAsState()
     val orderCodes = state.cache.orders.map { it.sku }.distinct()
-    var orderCode: String by remember { mutableStateOf("Select an order") }
+    var orderCode: String by remember { mutableStateOf(MainScreenMessages.DROPDOWN_INITIAL.message) }
     var expanded: Boolean by remember { mutableStateOf(false) }
 
-    Box {
+    Box(modifier) {
         Row(
-            modifier
-                .fillMaxWidth()
+            Modifier
                 .clickable {
                     expanded = !expanded
                 },
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = orderCode)
+            if (orderCode == MainScreenMessages.DROPDOWN_INITIAL.message) {
+                Text(
+                    text = orderCode,
+                    color = Color.Gray,
+                    modifier = Modifier.alpha(0.7f)
+                )
+            } else Text(
+                orderCode
+            )
             Icon(imageVector = Icons.Filled.ArrowDropDown, null)
             DropdownMenu(
                 expanded = expanded,
                 onDismissRequest = { expanded = false },
-                modifier = Modifier.height(200.dp)
+                modifier = Modifier.height(300.dp)
             ) {
                 orderCodes.forEach { order ->
                     val isSelected = orderCode == order
