ca8f7104c36cb6f5e3bdd2c1c31de5b838030ad6
Xoan Gonzalez <xoangon@gmail.com>
2022-04-17 10:08:29 +0200

    Test for retrieve orders JSON

--- app/src/test/java/com/example/sample_sales_app/TotalAmountTest.kt
+++ app/src/test/java/com/example/sample_sales_app/TotalAmountTest.kt
@@ -1,17 +1,40 @@
 package com.example.sample_sales_app
 
+import com.example.sample_sales_app.data_model.Currency.*
+import com.example.sample_sales_app.data_model.Order
+import com.example.sample_sales_app.utils.deserialize
+import io.kotest.matchers.shouldBe
+import org.junit.Rule
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class TotalAmountTest {
-    val sampleOrder = """ 
-        {"sku": "T2006", "amount": "10.00", "currency": "USD"},
-        {"sku": "T2006", "amount": "7.63", "currency": "EUR"}
+    private val sampleOrder = """ 
+        [{"sku": "T2006", "amount": "10.00", "currency": "USD"},
+        {"sku": "T2006", "amount": "7.63", "currency": "EUR"}]
         """.trimIndent()
 
-    @DisplayName("Retrieves the data for sampleOrder")
-    @Test
-    fun `properly retrieves the data`() {
+    @DisplayName("Retrieves the data for")
+    @Nested
+    inner class RetrieveData {
 
+        @get:Rule
+        val ordersData: List<Order> = sampleOrder.deserialize()
+
+        @DisplayName("first order")
+        @Test
+        fun `properly retrieves first order data`() {
+            ordersData[0] shouldBe Order("T2006", "10.00", USD)
+        }
+
+        @DisplayName("second order")
+        @Test
+        fun `properly retrieves first second data`() {
+            val ordersData: List<Order> = sampleOrder.deserialize()
+            ordersData[1] shouldBe Order("T2006", "7.63", EUR)
+        }
     }
+
+    
 }
